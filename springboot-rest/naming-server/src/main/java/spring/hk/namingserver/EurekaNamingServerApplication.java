fb426940816c7baed8377d54761f0badb8a86b1e
ohm2303 <95691732+ohm2303@users.noreply.github.com>
2023-10-12 00:38:44 +0700

    update eureka created

--- springboot-rest/naming-server/src/main/java/spring/hk/namingserver/EurekaNamingServerApplication.java
+++ springboot-rest/naming-server/src/main/java/spring/hk/namingserver/EurekaNamingServerApplication.java
@@ -2,12 +2,14 @@ package spring.hk.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class NamingServerApplication {
+@EnableEurekaServer
+public class EurekaNamingServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NamingServerApplication.class, args);
+		SpringApplication.run(EurekaNamingServerApplication.class, args);
 	}
 
 }
