20df210e24c4b6fc37093814e394790a86b806ec
Srikarette <opkungkeke@gmail.com>
2023-10-12 00:15:25 +0700

    Notification update

--- react-native-app/components/Notification.js
+++ react-native-app/components/Notification.js
@@ -1,18 +1,17 @@
 import { useState, useEffect, useRef } from 'react';
-import { Text, View, Button, Platform, StyleSheet, StatusBar } from 'react-native';
-import * as Notifications from 'expo-notifications';
+import { Text, View, Button, Platform } from 'react-native';
 import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
-    shouldPlaySound: false,
+    shouldPlaySound: true,
     shouldSetBadge: false,
   }),
 });
 
 export default function App() {
-
   const [expoPushToken, setExpoPushToken] = useState('');
   const [notification, setNotification] = useState(false);
   const notificationListener = useRef();
@@ -35,27 +34,39 @@ export default function App() {
     };
   }, []);
 
-
-  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
       <Text>Your expo push token: {expoPushToken}</Text>
-      <Button title="Click Me" onPress={() => alert('Button clicked!')} />
-      <StatusBar style="auto" />
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
     </View>
   );
-
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "This is just Notification test!! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 1 },
+  });
+}
 
 async function registerForPushNotificationsAsync() {
   let token;
@@ -80,9 +91,7 @@ async function registerForPushNotificationsAsync() {
       alert('Failed to get push token for push notification!');
       return;
     }
-    // Learn more about projectId:
-    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-    token = (await Notifications.getExpoPushTokenAsync({ projectId: 'your-project-id' })).data;
+    token = (await Notifications.getExpoPushTokenAsync()).data;
     console.log(token);
   } else {
     alert('Must use physical device for Push Notifications');
@@ -90,4 +99,3 @@ async function registerForPushNotificationsAsync() {
 
   return token;
 }
-
