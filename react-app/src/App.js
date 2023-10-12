1bdf98bc5f9cbd723d48d212da69f1d56ef8c74c
Potterpurpp <118960067+Potterpurpp@users.noreply.github.com>
2023-10-12 14:22:33 +0700

    Start navbar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import ComponentContainer from "./components/ComponentContainer";
 import GlobalStyle from "./components/GlobalStyle";
 import Map from "./components/Map";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <>
+    <Navbar />
       <GlobalStyle />
         <Map />
     </>
---
+++ react-app/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Navbar = ({className}) => {
+  return (
+    <div className={className}>
+    
+    </div>
+  )
+}
+export default styled(Navbar)`
+
+`
