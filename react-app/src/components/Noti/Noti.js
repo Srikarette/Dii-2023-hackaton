187e1298147a995430ff616e878427d9ae05f338
Potterpurpp <118960067+Potterpurpp@users.noreply.github.com>
2023-10-12 17:26:54 +0700

    Navbar: success but not beautiful

---
+++ react-app/src/components/Noti/Noti.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Noti = ({className}) => {
+  return (
+    <div className={className}>
+        
+    </div>
+  )
+}
+export default styled(Noti)`
+
+`
