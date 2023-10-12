806ec6b4d391fec70026a43e9cdfdde5d1d64cb7
Srikarette <opkungkeke@gmail.com>
2023-10-12 16:45:55 +0700

    update map component

--- react-app/src/components/Map.js
+++ react-app/src/components/Map.js
@@ -1,24 +1,37 @@
 import { React, useState,useEffect } from 'react';
-import { MapContainer, TileLayer, Circle,Marker ,Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Circle,Marker ,Popup,Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import Navbar from './navbar/Navbar';
 
 function Map({ className }) {
     const initialCenter = [13.7563, 100.5018]; // Thailand's coordinates
     const initialZoomLevel = 6; // Initial zoom level
-    const customMarkerIcon = L.divIcon({
+
+    const southwestBound = L.latLng(5, 90); // Define the southwest corner of the allowed area
+    const northeastBound = L.latLng(25, 120); // Define the northeast corner of the allowed area
+    const bounds = L.latLngBounds(southwestBound, northeastBound); // Create a bounds object
+
+
+    const dangerZoneMarker = L.divIcon({
         className: 'custom-marker-icon',
         html: '<div style="background-color: red; width: 12px; height: 12px; border-radius: 50%;"></div>',
+    });
+
+    const userLocationMarker = L.divIcon({
+        className: 'custom-marker-icon',
+        html: '<div style="background-color: blue; width: 12px; height: 12px; border-radius: 50%;"></div>',
     });
 
     const [userLocation, setUserLocation] = useState(null);
     const [zoom, setZoom] = useState(initialZoomLevel);
     const [center, setCenter] = useState(initialCenter);
     const [mapReady, setMapReady] = useState(false);
-    const [showMarker, setShowMarker] = useState(false);
+    
+    const [showDangerZone, setShowDangerZone] = useState(false);
+    const [showUserLocation, setShowUserLocation] = useState(false);
+    const [dangerZoneRadius, setDangerZoneRadius] = useState(1000); // Default radius in meters (1 kilometer)
 
     // Function to handle clicking the "Go to My Location" button
     const handleGoToUserLocation = () => {
@@ -27,8 +40,9 @@ function Map({ className }) {
                 const { latitude, longitude } = position.coords;
                 setUserLocation([latitude, longitude]);
                 setCenter([latitude, longitude]); // Set the center to the user's location
-                setZoom(18); // Set a custom zoom level to zoom in on the user's location
-                setShowMarker(true); // Show the marker
+                setZoom(16); // Set a custom zoom level to zoom in on the user's location
+                setShowDangerZone(!showDangerZone); // Show the danger zone
+                setShowUserLocation(!showUserLocation)
             });
         } else {
             alert('Geolocation is not supported in your browser.');
@@ -42,8 +56,8 @@ function Map({ className }) {
                     const { latitude, longitude } = position.coords;
                     setUserLocation([latitude, longitude]);
                     setCenter([latitude, longitude]);
-                    setZoom(17);
-                    setShowMarker(false);
+                    setZoom(16);
+                    setShowUserLocation(true);
                     setMapReady(true); // Set map readiness to true
                 });
             } else {
@@ -54,37 +68,60 @@ function Map({ className }) {
         fetchUserLocation();
     }, []);
 
+    // Function to update the danger zone radius
+    const handleRadiusChange = (event) => {
+        const newRadiusInKilometers = parseFloat(event.target.value);
+        const newRadiusInMeters = newRadiusInKilometers * 1000; // Convert to meters
+        setDangerZoneRadius(newRadiusInMeters);
+    };
+
     return (
-        <>
-         <Navbar />
         <div className={className}>
             <div className="playground">
-                <button className='emergency-btn' onClick={handleGoToUserLocation}>Emergency aleart!</button>
+                <button className='emergency-btn' onClick={handleGoToUserLocation}>Emergency Alert!</button>
+                {/* <div className='radius-control'>
+                    <label>Set Danger Zone Radius (kilometers):</label>
+                    <input type="number" min="1" step="0.1" value={dangerZoneRadius / 1000} onChange={handleRadiusChange} />
+                </div> */}
                 <div className='info'>
                     {userLocation && (
-                        <MapContainer center={center} zoom={zoom} className="MapContainer">
+                        <MapContainer center={center} zoom={zoom} className="MapContainer" bounds={bounds} minZoom={6}>
                             <TileLayer
                                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             />
-                            {showMarker && (
+                            {showDangerZone && (
                                 <>
-                                    <Circle center={userLocation} radius={100} color="red">
-                                        <Popup>You are here</Popup>
+                                   <Circle center={userLocation} radius={dangerZoneRadius} color="red" stroke={0}>
+                                        <Tooltip permanent>
+                                            <div className='disaster-info'>
+                                                Test Disaster<br />
+                                                {dangerZoneRadius / 1000} km
+                                            </div>
+                                        </Tooltip>
+                                        <Popup>Dangerzone</Popup>
                                     </Circle>
-                                    <Marker position={userLocation} icon={customMarkerIcon}>
-                                        <Popup>Center</Popup>
+                                    <Marker position={userLocation} icon={dangerZoneMarker}>
+                                        <Popup>You are here</Popup>
                                     </Marker>
                                 </>
+                            )}
+                            {showUserLocation && (
+                                <Marker position={userLocation} icon={userLocationMarker}>
+                                    <Popup>Your Location</Popup>
+                                </Marker>
                             )}
                         </MapContainer>
                     )}
                 </div>
             </div>
         </div>
-        </>
-       
     );
+}
+
+
+Map.propTypes = {
+    className: PropTypes.string.isRequired
 }
 
 
@@ -95,19 +132,30 @@ Map.propTypes = {
 export default styled(Map)`
 
 .MapContainer {
-
+    margin-top: 9vh;
     width: 100%;
-    height: 1000px;
+    height: calc(100vh - 64px); /* Adjust height to make space for the button */
 }
-.info {
 
+.info {
+    position: relative;
 }
+
 .playground {
     width: 100%;
-    margin-top: 40px;
+    position: relative;
 }
-.emergency-btn{
-    margin-left: 50%;
+
+.emergency-btn {
+    position: absolute;
+    bottom: 0;
+    left: 50%; /* Center the button horizontally */
+    transform: translateX(-50%); /* Center the button horizontally */
+    z-index: 999; /* Set a higher z-index to ensure the button is displayed above the map */
+    margin-bottom: 3%;
+    border: 1px solid black;
+    background-color: red;
+    text-align:center;
 }
 
 .custom-marker-icon {
