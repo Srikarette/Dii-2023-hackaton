2c2b574e0e06782b7373d52e8ca2d2172f207ff8
Potterpurpp <118960067+Potterpurpp@users.noreply.github.com>
2023-10-16 22:16:38 +0700

    NPM: ChakraProvider

--- react-app/backend/Server.js
+++ react-app/backend/Server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv"); // Can open many tab for 1 port
 const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Api is running");
@@ -13,9 +15,10 @@ app.get("/api/chat", (req, res) => {
 
 app.get("/api/chat/:id", (req, res) => {
   // console.log(req.params.id);
-
   const singleChat = chats.find((c) => c._id === req.params.id);
   res.send(singleChat);
 });
 
-app.listen(8000, console.log("PORT 5000 start"));
+const PORT = process.env.PORT || 9000;
+
+app.listen(9000, console.log(`PORT ${PORT} start`));
