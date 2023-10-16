f7610a4d72720ea34b75ad8fa0756e1a98336c9d
Potterpurpp <118960067+Potterpurpp@users.noreply.github.com>
2023-10-16 23:55:48 +0700

    NPM: router-dom V.5 and axios

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,49 +3,19 @@ import React from "react";
 import ComponentContainer from "./components/ComponentContainer";
 import GlobalStyle from "./components/GlobalStyle";
 import Map from "./components/Map";
-import {
-  createBrowserRouter,
-  Route,
-  createRoutesFromElements,
-  Link,
-  Outlet,
-  RouterProvider,
-  Switch,
-} from "react-router-dom";
 import Chatpage from "./pages/Chatpage";
 import Erro404 from "./pages/Erro404";
+import { Route } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root />}>
-        <Route index element={<Map />} />
-        <Route path="/chat" element={<Chatpage />} />
-        <Route path="/404" element={<Erro404 />} />
-      </Route>
-    )
-  );
-
   return (
     <>
       <div className="App">
-        <RouterProvider router={router} />
-        <GlobalStyle />
+        <Route path="/" component={Map} exact />
+        <Route path="/chat" component={Chatpage} />
       </div>
     </>
   );
 }
-const Root = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <Switch>
-        <Route exact path="/" component={Map} />
-        <Route path="/chat" component={Chatpage} />
-        <Route component={Erro404} />
-      </Switch>
-    </>
-  );
-};
 
 export default App;
