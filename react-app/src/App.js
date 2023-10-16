d63465e09af983edac4abf6f86b529a0a93dcd42
Potterpurpp <118960067+Potterpurpp@users.noreply.github.com>
2023-10-16 21:32:06 +0700

    Save cache

---
+++ react-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ComponentContainer from "./components/ComponentContainer";
+import GlobalStyle from "./components/GlobalStyle";
+import Map from "./components/Map";
+
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+        <Map />
+    </>
+  );
+}
+
+export default App;
