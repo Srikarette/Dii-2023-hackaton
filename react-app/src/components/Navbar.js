9f3573824aea6db068c68c3a02b607ee29802457
Potterpurpp <potterpurpp69@gmail.com>
2023-10-22 22:46:51 +0700

    New navbar style

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -1,63 +1,29 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 import Logo from "../assets/LogoAlertTown 2.png";
-import { GrNotification } from "react-icons/gr";
-
-export function Navbar({ className }) {
+export function Navbar() {
   return (
     <>
-      <div className={className}>
-        <nav>
-          <Link to="/">
-            <img src={Logo} className="logoimg" alt="LogoPng" />
-          </Link>
-          <ul>
-            <li>
-              <Link to="/notifications" className="notification-link">
-                Notification <GrNotification className="notification-icon" />
-              </Link>
-            </li>
-            <li>
-              <Link to="/officials">For officials</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <nav className="text-white bg bg-red-600 flex justify-between items-center">
+        <Link to="/">
+          <img
+            src={Logo}
+            className="object-cover w-24 h-22 rounded-full bg-gradient-to-r from-purple-400 to-pink-300 ml-4"
+            alt="LogoPng"
+          />
+        </Link>
+        <ul className="flex">
+          <li className="list-none mx-3.5">
+            <Link
+              to="/officials"
+              className="border-2 border-rose-500 bg-slate-600 p-1 rounded-lg"
+            >
+              For officials
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
-
-Navbar.propTypes = {};
-
-export default styled(Navbar)`
-  nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: white;
-    background-color: red;
-  }
-  nav ul {
-    display: flex;
-  }
-  li {
-    list-style: none;
-    margin: 0px 15px;
-  }
-  .logoimg {
-    width: 100px;
-    height: 87px; // Adjust to an appropriate value
-    object-fit: cover;
-  }
-  .notification-link {
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    color: white;
-  }
-  .notification-icon {
-    margin-left: 5px; // Adjust the spacing as needed
-  }
-`;
+export default Navbar;
