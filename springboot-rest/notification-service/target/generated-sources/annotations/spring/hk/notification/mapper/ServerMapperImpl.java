722dbbb38b01285be131f5c16302863c13625b1d
Srikarette <opkungkeke@gmail.com>
2023-10-30 14:50:46 +0700

    update module

--- springboot-rest/notification-service/target/generated-sources/annotations/spring/hk/notification/mapper/ServerMapperImpl.java
+++ springboot-rest/notification-service/target/generated-sources/annotations/spring/hk/notification/mapper/ServerMapperImpl.java
@@ -9,7 +9,7 @@ import spring.hk.notification.model.Notification;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-30T13:36:18+0700",
+    date = "2023-10-30T14:39:17+0700",
     comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.35.0.v20230814-2020, environment: Java 17.0.8.1 (Eclipse Adoptium)"
 )
 @Component
