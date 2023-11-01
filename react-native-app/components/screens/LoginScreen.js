69bd891e37d0771509bc78408f5c05fc1e81cfdd
Srikarette <opkungkeke@gmail.com>
2023-11-01 12:54:20 +0700

    update eas.json

--- react-native-app/components/screens/LoginScreen.js
+++ react-native-app/components/screens/LoginScreen.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import LineLogin from 'react-native-line-sdk';
+// import React from 'react';
+// import { View, Text, TouchableOpacity } from 'react-native';
+// import LineLogin from 'react-native-line-sdk';
 
-export default function LoginScreen () {
-  const channel_id = '2001383418'
+// export default function LoginScreen () {
+//   const channel_id = '2001383418'
   
-  const handleLineLogin = async () => {
-    try {
-      const loginResult = await LineLogin.login({
-        channelID: channel_id,
-      });
+//   const handleLineLogin = async () => {
+//     try {
+//       const loginResult = await LineLogin.login({
+//         channelID: channel_id,
+//       });
 
-      if (loginResult && loginResult.id_token) {
-        // User is logged in, and you can get the user's ID token here.
-        const idToken = loginResult.id_token;
-        console.log('Logged in with Line:', idToken);
-        // You can now send this token to your backend for user validation.
-      }
-    } catch (error) {
-      console.error('Line Login failed:', error);
-    }
-  };
+//       if (loginResult && loginResult.id_token) {
+//         // User is logged in, and you can get the user's ID token here.
+//         const idToken = loginResult.id_token;
+//         console.log('Logged in with Line:', idToken);
+//         // You can now send this token to your backend for user validation.
+//       }
+//     } catch (error) {
+//       console.error('Line Login failed:', error);
+//     }
+//   };
 
-  return (
-    <View>
-      <TouchableOpacity onPress={handleLineLogin}>
-        <Text>Log in with Line</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+//   return (
+//     <View>
+//       <TouchableOpacity onPress={handleLineLogin}>
+//         <Text>Log in with Line</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// };
 
