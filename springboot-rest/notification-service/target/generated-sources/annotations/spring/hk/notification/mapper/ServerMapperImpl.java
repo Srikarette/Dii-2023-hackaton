48260560f16405da0fa33ca350f50d56b75c6fa8
Srikarette <opkungkeke@gmail.com>
2023-11-03 20:53:14 +0700

    prepare for merge

--- springboot-rest/notification-service/target/generated-sources/annotations/spring/hk/notification/mapper/ServerMapperImpl.java
+++ springboot-rest/notification-service/target/generated-sources/annotations/spring/hk/notification/mapper/ServerMapperImpl.java
@@ -9,7 +9,7 @@ import spring.hk.notification.model.Notification;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-01T14:36:34+0700",
+    date = "2023-11-03T20:50:45+0700",
     comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.35.0.v20230814-2020, environment: Java 17.0.8.1 (Eclipse Adoptium)"
 )
 @Component
