e6149e60eb99aa7b2e839cbe08daa0ee78c2dc4c
ohm2303 <95691732+ohm2303@users.noreply.github.com>
2023-11-04 18:05:35 +0700

    create Admin manage

---
+++ react-app/src/pages/ManageAdmin.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Admin(){
+
+
+    // return 
+    return(
+        <>
+            <h1>this page is show admin</h1>
+        </>
+    );
+}
+
+export default Admin;
--- springboot-rest/notification-service/target/generated-sources/annotations/spring/hk/notification/mapper/ServerMapperImpl.java
+++ springboot-rest/notification-service/target/generated-sources/annotations/spring/hk/notification/mapper/ServerMapperImpl.java
@@ -9,7 +9,7 @@ import spring.hk.notification.model.Notification;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-01T14:36:34+0700",
+    date = "2566-11-04T18:02:34+0700",
     comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.35.0.v20230814-2020, environment: Java 17.0.8.1 (Eclipse Adoptium)"
 )
 @Component
